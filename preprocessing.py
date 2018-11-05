9356487e434850fee84c97c00057332505a2fc8b
Hussein Azeez <hussein.azeez91@gmail.com>
2018-11-05 11:41:31 +0700

    Create preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,14 +1,93 @@
 from PIL import Image
+import cv2
+import numpy as np
+import math
+from scipy import ndimage
 
-test_image = "./testset/5.png"
-original = Image.open(test_image)
-original.show()
+def getBestShift(img):
+    cy, cx = ndimage.measurements.center_of_mass(img)
+    rows, cols = img.shape
+    shiftx = np.round(cols/2.0-cx).astype(int)
+    shifty = np.round(rows/2.0-cy).astype(int)
 
-width, height = original.size   # Get dimensions
-left = width/4
-top = height/4
-right = 3 * width/4
-bottom = 3 * height/4
-cropped_example = original.crop((left, top, right, bottom))
+    return shiftx, shifty
 
-cropped_example.show()
+
+def shift(img, sx, sy):
+    rows, cols = img.shape
+    M = np.float32([[1, 0, sx], [0, 1, sy]])
+    shifted = cv2.warpAffine(img, M, (cols, rows))
+    return shifted
+
+
+# create an array where we can store our 4 pictures
+images = np.zeros((10, 784))
+# and the correct values
+correct_vals = np.zeros((10, 10))
+
+# we want to test our images which you saw at the top of this page
+i = 0
+for no in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]:
+    # read the image
+    gray = cv2.imread("./data/testing/"+str(no)+".jpg", cv2.IMREAD_GRAYSCALE)
+    print(gray)
+
+    # resize the images and invert it (black background)
+    gray = cv2.resize(255 - gray, (28, 28))
+    (thresh, gray) = cv2.threshold(gray, 128,
+     255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+    while np.sum(gray[0]) == 0:
+        gray = gray[1:]
+
+    while np.sum(gray[:,0]) == 0:
+        gray = np.delete(gray,0,1)
+
+    while np.sum(gray[-1]) == 0:
+        gray = gray[:-1]
+
+    while np.sum(gray[:,-1]) == 0:
+        gray = np.delete(gray,-1,1)
+
+    rows,cols = gray.shape
+
+    if rows > cols:
+        factor = 20.0/rows
+        rows = 20
+        cols = int(round(cols*factor))
+        # first cols than rows
+        gray = cv2.resize(gray, (cols,rows))
+    else:
+        factor = 20.0/cols
+        cols = 20
+        rows = int(round(rows*factor))
+        # first cols than rows
+        gray = cv2.resize(gray, (cols, rows))
+
+    colsPadding = (int(math.ceil((28-cols)/2.0)),int(math.floor((28-cols)/2.0)))
+    rowsPadding = (int(math.ceil((28-rows)/2.0)),int(math.floor((28-rows)/2.0)))
+    gray = np.lib.pad(gray,(rowsPadding,colsPadding),'constant')
+
+    shiftx,shifty = getBestShift(gray)
+    shifted = shift(gray,shiftx,shifty)
+    gray = shifted
+    # save the processed images
+    cv2.imwrite("./data/preprocessed/"+str(no)+".jpg", gray)
+    """
+    all images in the training set have an range from 0-1
+    and not from 0-255 so we divide our flatten images
+    (a one dimensional vector with our 784 pixels)
+    to use the same 0-1 based range
+    """
+    flatten = gray.flatten() / 255.0
+    """
+    we need to store the flatten image and generate
+    the correct_vals array
+    correct_val for the first digit (9) would be
+    [0,0,0,0,0,0,0,0,0,1]
+    """
+    images[i] = flatten
+    correct_val = np.zeros((10))
+    correct_val[no] = 1
+    correct_vals[i] = correct_val
+    i += 1
