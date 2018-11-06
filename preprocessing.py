c7e3738e52b370ca3792747b300d7b012d31574e
Hussein Azeez <hussein.azeez91@gmail.com>
2018-11-06 17:02:01 +0700

    Remove grayscale step

--- preprocessing.py
+++ preprocessing.py
@@ -21,68 +21,66 @@ def shift(img, sx, sy):
     return shifted
 
 
-# create an array where we can store our 4 pictures
-images = np.zeros((13, 784))
+# create an array where we can store our 10 pictures
+images = np.zeros((10, 784))
 # and the correct values
-correct_vals = np.zeros((13, 10))
+correct_vals = np.zeros((10, 10))
 
 # we want to test our images which you saw at the top of this page
 i = 0
-for no in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]:
+for no in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
     # read the image
-    gray = cv2.imread("./data/testing/"+str(no)+".jpg", cv2.IMREAD_GRAYSCALE)
-    print(gray)
+    image = cv2.imread("./data/digits/"+str(no)+".png", cv2.IMREAD_UNCHANGED)
+    print(image)
 
     # resize the images and invert it (black background)
-    gray = cv2.resize(255 - gray, (28, 28))
-    (thresh, gray) = cv2.threshold(gray, 128,
-                                   255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    image = cv2.resize(255 - image, (28, 28))
+    print(image.shape)
+    while np.sum(image[0]) == 0:
+        image = image[1:]
 
-    while np.sum(gray[0]) == 0:
-        gray = gray[1:]
+    while np.sum(image[:, 0]) == 0:
+        image = np.delete(image, 0, 1)
 
-    while np.sum(gray[:, 0]) == 0:
-        gray = np.delete(gray, 0, 1)
+    while np.sum(image[-1]) == 0:
+        image = image[:-1]
 
-    while np.sum(gray[-1]) == 0:
-        gray = gray[:-1]
+    while np.sum(image[:, -1]) == 0:
+        image = np.delete(image, -1, 1)
 
-    while np.sum(gray[:, -1]) == 0:
-        gray = np.delete(gray, -1, 1)
-
-    rows, cols = gray.shape
+    rows, cols = image.shape
 
     if rows > cols:
         factor = 20.0/rows
         rows = 20
         cols = int(round(cols*factor))
         # first cols than rows
-        gray = cv2.resize(gray, (cols, rows))
+        image = cv2.resize(image, (cols, rows))
     else:
         factor = 20.0/cols
         cols = 20
         rows = int(round(rows*factor))
         # first cols than rows
-        gray = cv2.resize(gray, (cols, rows))
+        image = cv2.resize(image, (cols, rows))
 
     colsPadding = (int(math.ceil((28-cols)/2.0)),
                    int(math.floor((28-cols)/2.0)))
     rowsPadding = (int(math.ceil((28-rows)/2.0)),
                    int(math.floor((28-rows)/2.0)))
-    gray = np.lib.pad(gray, (rowsPadding, colsPadding), 'constant')
+    image = np.lib.pad(image, (rowsPadding, colsPadding), 'constant')
 
-    shiftx, shifty = getBestShift(gray)
-    shifted = shift(gray, shiftx, shifty)
-    gray = shifted
+    shiftx, shifty = getBestShift(image)
+    shifted = shift(image, shiftx, shifty)
+    image = shifted
     # save the processed images
-    cv2.imwrite("./data/preprocessed/"+str(no)+".jpg", gray)
+    cv2.imwrite("./data/preprocessed/"+str(no)+".png", image)
     """
     all images in the training set have an range from 0-1
     and not from 0-255 so we divide our flatten images
     (a one dimensional vector with our 784 pixels)
     to use the same 0-1 based range
     """
-    flatten = gray.flatten() / 255.0
+    flatten = image.flatten() / 255.0
     """
     we need to store the flatten image and generate
     the correct_vals array
