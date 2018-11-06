f052e6ac689a1251c4e3f0558d6092b6c423934c
Hussein Azeez <hussein.azeez91@gmail.com>
2018-11-06 10:23:04 +0700

    Apply PEP 8 style

--- preprocessing.py
+++ preprocessing.py
@@ -4,6 +4,7 @@ import numpy as np
 import math
 from scipy import ndimage
 
+
 def getBestShift(img):
     cy, cx = ndimage.measurements.center_of_mass(img)
     rows, cols = img.shape
@@ -21,13 +22,13 @@ def shift(img, sx, sy):
 
 
 # create an array where we can store our 4 pictures
-images = np.zeros((10, 784))
+images = np.zeros((13, 784))
 # and the correct values
-correct_vals = np.zeros((10, 10))
+correct_vals = np.zeros((13, 10))
 
 # we want to test our images which you saw at the top of this page
 i = 0
-for no in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]:
+for no in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]:
     # read the image
     gray = cv2.imread("./data/testing/"+str(no)+".jpg", cv2.IMREAD_GRAYSCALE)
     print(gray)
@@ -35,28 +36,28 @@ for no in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]:
     # resize the images and invert it (black background)
     gray = cv2.resize(255 - gray, (28, 28))
     (thresh, gray) = cv2.threshold(gray, 128,
-     255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+                                   255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
     while np.sum(gray[0]) == 0:
         gray = gray[1:]
 
-    while np.sum(gray[:,0]) == 0:
-        gray = np.delete(gray,0,1)
+    while np.sum(gray[:, 0]) == 0:
+        gray = np.delete(gray, 0, 1)
 
     while np.sum(gray[-1]) == 0:
         gray = gray[:-1]
 
-    while np.sum(gray[:,-1]) == 0:
-        gray = np.delete(gray,-1,1)
+    while np.sum(gray[:, -1]) == 0:
+        gray = np.delete(gray, -1, 1)
 
-    rows,cols = gray.shape
+    rows, cols = gray.shape
 
     if rows > cols:
         factor = 20.0/rows
         rows = 20
         cols = int(round(cols*factor))
         # first cols than rows
-        gray = cv2.resize(gray, (cols,rows))
+        gray = cv2.resize(gray, (cols, rows))
     else:
         factor = 20.0/cols
         cols = 20
@@ -64,12 +65,14 @@ for no in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]:
         # first cols than rows
         gray = cv2.resize(gray, (cols, rows))
 
-    colsPadding = (int(math.ceil((28-cols)/2.0)),int(math.floor((28-cols)/2.0)))
-    rowsPadding = (int(math.ceil((28-rows)/2.0)),int(math.floor((28-rows)/2.0)))
-    gray = np.lib.pad(gray,(rowsPadding,colsPadding),'constant')
+    colsPadding = (int(math.ceil((28-cols)/2.0)),
+                   int(math.floor((28-cols)/2.0)))
+    rowsPadding = (int(math.ceil((28-rows)/2.0)),
+                   int(math.floor((28-rows)/2.0)))
+    gray = np.lib.pad(gray, (rowsPadding, colsPadding), 'constant')
 
-    shiftx,shifty = getBestShift(gray)
-    shifted = shift(gray,shiftx,shifty)
+    shiftx, shifty = getBestShift(gray)
+    shifted = shift(gray, shiftx, shifty)
     gray = shifted
     # save the processed images
     cv2.imwrite("./data/preprocessed/"+str(no)+".jpg", gray)
