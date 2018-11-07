83ccb046ae135592b238720d82986cf3652fea12
Hussein Azeez <hussein.azeez91@gmail.com>
2018-11-07 16:58:00 +0700

    Fix threshold type

--- segmentation.py
+++ segmentation.py
@@ -1,23 +1,19 @@
 import cv2
-from PIL import Image, ImageEnhance
-import pytesseract as tes
 import numpy as np
-from skimage import io
 
-image = cv2.imread('./pdf/19.tiff')
-gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-blur = cv2.GaussianBlur(gray, (5, 5), 0)
-thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 75, 10)
+image = cv2.imread('./data/testing/out1.png', cv2.IMREAD_GRAYSCALE)
+blur = cv2.GaussianBlur(image, (9, 9), 0)
+thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 75, 40)
 bit = cv2.bitwise_not(thresh)
-_, contours, hierarchy = cv2.findContours(bit, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_KCOS)
-cv2.contourArea
+_, contours, hierarchy = cv2.findContours(bit, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+0
 for i in range(0, len(contours)):
     cnt = contours[i]
     x, y, w, h = cv2.boundingRect(cnt)
-    cv2.rectangle(bit, (x, y), (x+w, y+h), (255, 255, 255), 1)
-    digit = bit[y:y+h, x:x+w]
-    # cv2.imwrite("./data/digits/"+str(i)+".png", digit)
-  
-cv2.imshow('Contour', bit)
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 1)
+    digit = image[y:y+h, x:x+w]
+    cv2.imwrite("./data/digits/"+str(i)+".png", digit)
+
+cv2.imshow('Final Image', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
