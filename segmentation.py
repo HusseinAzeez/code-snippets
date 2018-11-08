8a807c8d10bfb2a43210dd023e8e14445c156646
Hussein Azeez <hussein.azeez91@gmail.com>
2018-11-08 22:33:37 +0700

    Change image threshold

--- segmentation.py
+++ segmentation.py
@@ -2,15 +2,15 @@ import cv2
 import numpy as np
 
 image = cv2.imread('./data/testing/out1.png', cv2.IMREAD_GRAYSCALE)
-blur = cv2.GaussianBlur(image, (9, 9), 0)
+blur = cv2.GaussianBlur(image, (15, 15), 0)
 thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 75, 40)
 bit = cv2.bitwise_not(thresh)
-_, contours, hierarchy = cv2.findContours(bit, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+_, contours, hierarchy = cv2.findContours(bit, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_TC89_KCOS)
 0
 for i in range(0, len(contours)):
     cnt = contours[i]
     x, y, w, h = cv2.boundingRect(cnt)
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 255), 1)
+    cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 255), 1)
     digit = image[y:y+h, x:x+w]
     cv2.imwrite("./data/digits/"+str(i)+".png", digit)
 
