f9fba26de6cbd503e4e6ceb0b3970c4385d7dc01
Hussein Azeez <hussein.azeez91@gmail.com>
2018-11-19 22:52:04 +0700

    Create script to concat two csv files

---
+++ concatenate.py
@@ -0,0 +1,26 @@
+
+import pandas as pd
+import glob
+import os
+
+
+def concatenate(outfile="./training.csv"):
+    fileList = glob.glob('../mnist-in-csv/*.csv')
+    dfList = []
+    colnames = []
+    for no in range(0, 784):
+        colnames.append('pixel ' + str(no))
+    colnames.append('label')
+
+    for filename in fileList:
+        df = pd.read_csv(filename, header=None)
+        dfList.append(df)
+    concatDf = pd.concat(dfList, axis=0)
+    concatDf.columns = colnames
+    concatDf.to_csv(outfile, index=False, encoding='utf-8')
+
+
+concatenate()
+
+df = pd.read_csv('./training.csv')
+print(df.iloc[:-1])
