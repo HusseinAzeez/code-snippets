1ba8679aa24f885a4ce0baf44ebbb85434ba5712
Hussein Azeez <hussein.azeez91@gmail.com>
2018-11-19 22:51:26 +0700

    Add new data to training set

--- train.py
+++ train.py
@@ -1,12 +1,7 @@
-'''Trains a simple convnet on the MNIST dataset.
-Gets to 99.25% test accuracy after 12 epochs
-(there is still a lot of margin for parameter tuning).
-16 seconds per epoch on a GRID K520 GPU.
-'''
-
 from __future__ import print_function
 
 import keras
+import pandas as pd
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
@@ -23,7 +18,17 @@ epochs = 10
 img_rows, img_cols = 28, 28
 
 # the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+train = pd.read_csv('./training.csv')
+test = pd.read_csv('./test.csv')
+x_train = train[:, 0:784]
+y_train = train[:, 785]
+x_test = test[:, 0:784]
+y_test = test[:, 785]
+print('X Train', x_train)
+print('Y Train', y_train)
+# print('X Train', x_train)
+
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
@@ -71,4 +76,4 @@ score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
-tfjs.converters.save_keras_model(model, "model_js")
+tfjs.converters.save_keras_model(model, "./model_js")
