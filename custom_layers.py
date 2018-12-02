a056dc666f59289548e08a5e44a4c6561bd2922f
Hussein Azeez <hussein.azeez91@gmail.com>
2018-12-02 21:57:45 +0700

    Change to Keras LRN2D

--- custom_layers.py
+++ custom_layers.py
@@ -1,39 +1,44 @@
 from keras.layers.core import Layer
-import theano.tensor as T
+from keras import backend as K
+import tensorflow as tf
+import numpy as np
 
 
-class LRN(Layer):
+class LRN2D(Layer):
 
-    def __init__(self, alpha=0.0001, k=1, beta=0.75, n=5, **kwargs):
+    def __init__(self, alpha=1e-4, k=2, beta=0.75, n=5, **kwargs):
+        if n % 2 == 0:
+            raise NotImplementedError(
+                "LRN2D only works with odd n. n provided: " + str(n))
+        super(LRN2D, self).__init__(**kwargs)
         self.alpha = alpha
         self.k = k
         self.beta = beta
         self.n = n
-        super(LRN, self).__init__(**kwargs)
 
-    def call(self, x, mask=None):
-        b, ch, r, c = x.shape
-        half_n = self.n // 2  # half the local region
-        input_sqr = T.sqr(x)  # square the input
-        # make an empty tensor with zero pads along channel dimension
-        extra_channels = T.alloc(0., b, ch + 2*half_n, r, c)
-        # set the center to be the squared input
-        input_sqr = T.set_subtensor(
-            extra_channels[:, half_n:half_n+ch, :, :], input_sqr)
-        scale = self.k  # offset for the scale
-        norm_alpha = self.alpha / self.n  # normalized alpha
+    def get_output(self, train):
+        X = self.get_input(train)
+        b, ch, r, c = K.shape(X)
+        half_n = self.n // 2
+        input_sqr = K.square(X)
+        extra_channels = K.zeros((b, ch + 2 * half_n, r, c))
+        input_sqr = K.concatenate([extra_channels[:, :half_n, :, :],
+                                   input_sqr,
+                                   extra_channels[:, half_n + ch:, :, :]],
+                                  axis=1)
+        scale = self.k
         for i in range(self.n):
-            scale += norm_alpha * input_sqr[:, i:i+ch, :, :]
+            scale += self.alpha * input_sqr[:, i:i + ch, :, :]
         scale = scale ** self.beta
-        x = x / scale
-        return x
+        return X / scale
 
     def get_config(self):
-        config = {"alpha": self.alpha,
+        config = {"name": self.__class__.__name__,
+                  "alpha": self.alpha,
                   "k": self.k,
                   "beta": self.beta,
                   "n": self.n}
-        base_config = super(LRN, self).get_config()
+        base_config = super(LRN2D, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
 
 
