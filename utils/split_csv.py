70586a6bc86971798f28d9cd55239313e162943e
Hussein Azeez <hussein.azeez91@gmail.com>
2018-12-13 16:44:44 +0700

    Delete split file

--- utils/split_csv.py
+++
@@ -1,6 +0,0 @@
-csvfile = open('./training3.csv', 'r').readlines()
-filename = 1
-for i in range(len(csvfile)):
-    if i % 5000 == 0:
-        open(str(filename) + '.csv', 'w+').writelines(csvfile[i:i+5000])
-        filename += 1
