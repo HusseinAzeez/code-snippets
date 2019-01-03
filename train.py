4352dcedcb89a21d1955912cf99f6367eab9c5d5
Hussein Azeez <hussein.azeez91@gmail.com>
2019-01-03 23:38:15 +0700

    Add methods docstrings

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 """
-    autour: Eraser (ตะวัน)
+    Autour: Eraser (ตะวัน)
 """
 
 # Local imports
@@ -9,6 +9,14 @@ from src.double_model import DoubleModel
 
 
 def train_length(path):
+    """Trains, evaluates, and save the length classifier.
+
+        Args:
+            path: A valid path to the full dataset.
+
+        Return:
+            None.
+    """
     length = LengthModel()
     x_train, y_train, x_val, y_val, x_test, y_test, input_shape = length.load_data(
         path)
@@ -18,6 +26,14 @@ def train_length(path):
 
 
 def train_single(path):
+    """Trains, evaluates, and save the single digit (0 pair) classifier.
+
+        Args:
+            path: A valid path to the full dataset.
+
+        Return:
+            None.
+    """
     single = SingleModel()
     x_train, y_train, x_val, y_val, x_test, y_test, input_shape = single.load_data(
         path)
@@ -27,6 +43,14 @@ def train_single(path):
 
 
 def train_double(path):
+    """Trains, evaluates, and save the double digit (1 pair) classifier.
+
+        Args:
+            path: A valid path to the full dataset.
+
+        Return:
+            None.
+    """
     double = DoubleModel()
     x_train, y_train, x_val, y_val, x_test, y_test, input_shape = double.load_data(
         path)
@@ -36,6 +60,15 @@ def train_double(path):
 
 
 def select_model(model_number, dataset_path):
+    """Reads the user inputs (dataset path, required model) for training.
+
+        Args:
+            model_number: The number of the required model for training.
+            dataset_path: A valid path to the full dataset.
+
+        Return:
+            None.
+    """
     if model_number == '0':
         train_length(dataset_path)
     elif model_number == '1':
@@ -47,10 +80,10 @@ def select_model(model_number, dataset_path):
 
 
 if __name__ == '__main__':
-    user_input_path = input(
+    USER_INPUT_PATH = input(
         '\n---------------- Please insert a valid dataset path ----------------\n path=: ')
-    user_input_model = input('''---------------- Please select a model to train ----------------\n
+    USER_INPUT_MODEL = input('''---------------- Please select a model to train ----------------\n
         0: To train the length model\n
         1: To train the single digit model\n
         2: To trian the double digit model\n model: ''')
-    select_model(model_number=user_input_model, dataset_path=user_input_path)
+    select_model(model_number=USER_INPUT_MODEL, dataset_path=USER_INPUT_PATH)
